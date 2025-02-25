e9166ef82c25753f295ed1371174b6a708b1bbb3
Deepraj <deepraj21.bera@gmail.com>
2025-02-26 00:27:03 +0530

    initial files

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import InfiniteMenu from './block/Components/InfiniteMenu/InfiniteMenu'
+
+function App() {
+  const items = [
+    {
+      image: 'https://picsum.photos/300/300?grayscale',
+      link: 'https://google.com/',
+      title: 'Item 1',
+      description: 'This is pretty cool, right?'
+    },
+    {
+      image: 'https://picsum.photos/400/400?grayscale',
+      link: 'https://google.com/',
+      title: 'Item 2',
+      description: 'This is pretty cool, right?'
+    },
+    {
+      image: 'https://picsum.photos/500/500?grayscale',
+      link: 'https://google.com/',
+      title: 'Item 3',
+      description: 'This is pretty cool, right?'
+    },
+    {
+      image: 'https://picsum.photos/600/600?grayscale',
+      link: 'https://google.com/',
+      title: 'Item 4',
+      description: 'This is pretty cool, right?'
+    }
+  ];
+
+  return (
+    <div className='h-full w-full'>
+      <InfiniteMenu items={items} />
+    </div>
+  )
+}
+
+export default App
